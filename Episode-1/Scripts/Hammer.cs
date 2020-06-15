1ce863173a678d7a398711800233e7b5a6ca4f95
NrdyBhu1 <61649925+NrdyBhu1@users.noreply.github.com>
2020-06-15 09:42:04 +0530

    Epidose-1
    Assets for episode one

---
+++ Episode-1/Scripts/Hammer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class Hammer : MonoBehaviour
+{
+    #region Variables
+    private Camera _camera;
+    private Vector2 _mousePosition;
+    // private Quaternion _moveRotation;
+    private float zRotation = 45f;
+    public Vector2 offset;
+    private Rigidbody2D _rb;
+    #endregion
+    
+    void Start()
+    {
+        _camera = FindObjectOfType<Camera>();
+        _rb = GetComponent<Rigidbody2D>();
+        // _moveRotation = new Quaternion(0f, 0f, zRotation);
+        // Debug.Log(_camera.name);
+
+    }
+
+    void FixedUpdate()
+    {
+        _mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+        _rb.MovePosition(_mousePosition + offset);
+        _rb.MoveRotation(new Quaternion(0f, 0f, 45f/4f, 0f));
+        // transform.position = _mousePosition;
+        // print(_mousePosition);
+    }
+}
