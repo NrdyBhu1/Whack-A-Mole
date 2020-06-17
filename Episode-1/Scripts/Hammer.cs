ed346d88616ed0dff2f0fda1dfdcee4fae97c279
NrdyBhu1 <61649925+NrdyBhu1@users.noreply.github.com>
2020-06-17 18:03:41 +0530

    Update Hammer.cs

--- Episode-1/Scripts/Hammer.cs
+++ Episode-1/Scripts/Hammer.cs
@@ -1,16 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
 
 public class Hammer : MonoBehaviour
 {
     #region Variables
     private Camera _camera;
     private Vector2 _mousePosition;
-    // private Quaternion _moveRotation;
-    private float zRotation = 45f;
     public Vector2 offset;
     private Rigidbody2D _rb;
     #endregion
@@ -19,7 +13,6 @@ public class Hammer : MonoBehaviour
     {
         _camera = FindObjectOfType<Camera>();
         _rb = GetComponent<Rigidbody2D>();
-        // _moveRotation = new Quaternion(0f, 0f, zRotation);
         // Debug.Log(_camera.name);
 
     }
@@ -28,7 +21,6 @@ public class Hammer : MonoBehaviour
     {
         _mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
         _rb.MovePosition(_mousePosition + offset);
-        _rb.MoveRotation(new Quaternion(0f, 0f, 45f/4f, 0f));
         // transform.position = _mousePosition;
         // print(_mousePosition);
     }
