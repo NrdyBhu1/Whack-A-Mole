6c8e4641582a078423b5eb4ecf6095ae95490fc1
NrdyBhu1 <61649925+NrdyBhu1@users.noreply.github.com>
2020-06-17 18:05:29 +0530

    Episode 2
    These are the project files for what was shown episode 2.

---
+++ Episode-2/Scripts/HammerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class HammerCollision : MonoBehaviour
+{
+    #region Variables
+    #endregion
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Mole"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
